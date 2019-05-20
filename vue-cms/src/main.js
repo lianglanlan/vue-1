cab92e8b306d921b21338e90152d06ebf7d9187b
lianglanlan <799679063@qq.com>
2019-05-20 16:22:22 +0800

    首页及横滑效果实现

--- vue-cms/src/main.js
+++ vue-cms/src/main.js
@@ -1,12 +1,28 @@
 //入口文件
 import Vue from 'vue'
 import App from './App.vue'
-import mui from './lib/mui/css/mui.min.css'
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+//路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router'
 
 //按需导入mint-ui中的组件
-import { Header } from 'mint-ui'
+import { Header,Swipe, SwipeItem, Toast } from 'mint-ui'
+
 Vue.component('mt-header', Header);
+Vue.component('mt-swipe', Swipe);
+Vue.component('mt-swipe-item', SwipeItem);
+Vue.prototype.$toast = Toast;
+
+//导入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 new Vue({
     el: '#app',
-    render:c=>c(App)
+    render:c=>c(App),
+    router
 })
